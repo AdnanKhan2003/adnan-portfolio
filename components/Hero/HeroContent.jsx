ab8a396deae8add9ce604cef117b0482bee38fff
Adnan <99629737+AdnanKhan2003@users.noreply.github.com>
2025-05-18 15:04:18 +0530

    Update HeroContent.jsx

--- components/Hero/HeroContent.jsx
+++ components/Hero/HeroContent.jsx
@@ -20,7 +20,7 @@ function HeroContent() {
         collaborations so feel free to contact me.
       </p>
       <Button
-        navigateto="https://drive.google.com/file/d/1vNRZm60IrIddLwEKGk12y88OzH5TxCKh/view?usp=sharing"
+        navigateto="https://drive.google.com/file/d/1OwlWMC4e3_qDpOGJyoqUwWBwYYG9Vrus/view?usp=sharing"
         size="big"
       >
         Resume

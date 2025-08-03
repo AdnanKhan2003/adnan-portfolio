c14c2b4f8e7c78cad27cf23246b3fac8bb09c9e6
Adnan <99629737+AdnanKhan2003@users.noreply.github.com>
2025-08-03 12:40:34 +0530

    Updated Drive Link Added to Header.jsx

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -119,7 +119,7 @@ function Header({ onClickScroll }, ref) {
           ref={resumeRef}
           size="med"
           text="button_text"
-          navigateto="https://drive.google.com/file/d/1OwlWMC4e3_qDpOGJyoqUwWBwYYG9Vrus/view?usp=sharing"
+          navigateto="https://drive.google.com/file/d/1rV-Efgb2EDXm45me127F-VUSpqFKr6Qb/view?usp=sharing "
           classes={`${styles.nav_right_btn}`}
         >
           Resume
